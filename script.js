8dcdc75b210353742da2dd979416ecdce2c91d50
theMorganObject <morgan.oshaughnessey@gmail.com>
2023-02-01 17:27:37 -1000

    integration test1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+console.log('continuous ONLINE');
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
